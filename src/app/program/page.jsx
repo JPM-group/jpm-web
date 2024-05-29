f675af577a88534e5d028bce15c3c9684011f669
jhagerman99 <106148658+jhagerman99@users.noreply.github.com>
2024-05-29 17:48:05 +0700

    update video in program page

--- src/app/program/page.jsx
+++ src/app/program/page.jsx
@@ -1,31 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
   return (
     <>
-      <heading className="mt-3 bg-center bg-no-repeat bg-[url('/bg_program.png')] bg-blend-multiply h-auto">
+      <heading className="mt-3 bg-center bg-no-repeat bg-[url('/bg-program.png')] bg-blend-multiply h-auto">
           <div className="ENG px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
               <h1 className="font-extralight tracking-wider text-2xl leading-none text-customBlue sm:text-4xl md:text-6xl lg:text-7xl">
                 Program development 
                 <span className='font-light ml-5 text-2xl sm:text-5xl md:text-7xl lg:text-8xl'>works</span>
               </h1>
-              <p class="mt-5 font-normal text-lg text-customBlue sm:text-xl md:text-2xl lg:text-3xl">interactive program & mini game for event</p>
+              <p className="mt-5 font-normal text-lg text-customBlue sm:text-xl md:text-2xl lg:text-3xl">interactive program & mini game for event</p>
           </div>
           <div className='ENG font-extralight gradient-tab text-lg'>Connecting Ideas, Creating Solutions</div>
       </heading>
 
       <project>
         <div>
-          <video width="320" height="240" controls preload="none">
-            <source src="/path/to/video.mp4" type="video/mp4" />
-            <track
-              src="/path/to/captions.vtt"
-              kind="subtitles"
-              srcLang="en"
-              label="English"
-            />
-            Your browser does not support the video tag.
-          </video>
+        <video width="320" height="240" controls preload="none">
+          <source src="/videos/v1.mp4" type="video/mp4" />
+        </video>
         </div>
         <div></div>
         <div></div>
