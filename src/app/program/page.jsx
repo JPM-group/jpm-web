b0e39bcd4b3706a702ccf7fc2cb65d2022acf134
jhagerman99 <106148658+jhagerman99@users.noreply.github.com>
2024-05-31 10:58:02 +0700

    fix bg-program problem

--- src/app/program/page.jsx
+++ src/app/program/page.jsx
@@ -5,18 +5,10 @@ import imagePath from '../program/bg-program.png'
 const page = () => {
   return (
     <>
-      <heading className="mt-3 bg-center">
-            <div className="absolute -z-10 inset-0">
-              <Image
-                src={imagePath}
-                alt="bg"
-                fill
-                style={{objectFit:'container'}}
-              />
-            </div>    
+      <heading className="mt-3 bg-center w-full block" style={{backgroundImage: `url(${imagePath.src})`, width:'100%!',height:'100%'}}>
           <div className="ENG px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
               <h1 className="font-extralight tracking-wider text-2xl leading-none text-customBlue sm:text-4xl md:text-6xl lg:text-7xl">
-                Program development 
+                Program development
                 <span className='font-light ml-5 text-2xl sm:text-5xl md:text-7xl lg:text-8xl'>works</span>
               </h1>
               <p className="mt-5 font-normal text-lg text-customBlue sm:text-xl md:text-2xl lg:text-3xl">interactive program & mini game for event</p>
